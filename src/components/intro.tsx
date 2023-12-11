7ce96b458d1670501da3caaec3166603a1f78b43
Muhammad Fikri Al-Barik <muhammadfikrialbarik@gmai.com>
2023-12-11 10:42:14 +0700

    fix download cv using gdrive

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -73,7 +73,7 @@ export default function Intro() {
             Contact me here <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition"/>
           </Link>
 
-          <a href="/cv.pdf" className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10" download>
+          <a href="https://drive.google.com/u/1/uc?id=1m1ASYi0mrk20zFoIakIu6ieSV2FUy1i2&export=download" className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10" download>
             Download CV <HiDownload className="opaxity-60 group-hover:translate-y-1 transition"/>
           </a>
 
